86334aba133d1161d9e3098a742285e90a78c401
FCjosh <47929869+FCjosh@users.noreply.github.com>
2022-05-01 13:00:37 -0600

    Odom and xDrive Working

--- 2022RobotCode/src/main.cpp
+++ 2022RobotCode/src/main.cpp
@@ -1,28 +1,48 @@
+//Upload from file directory with normal terminal using 'prosv5 mu --slot n'
+#include <string>
 #include "main.h"
 #include "okapi/api.hpp"
 using namespace okapi;
 void opcontrol() {
-	std::shared_ptr<ChassisController> drive =
+	auto chassis =
 		ChassisControllerBuilder()
-			.withMotors(1, -2, 3, 4) // topleft motor is 1, top right motor is 2 (reversed), bottom right is 3, bottom left is 4
+			.withMotors(18, -20, -19, 17) // topleft motor is 18, top right motor is 20 (reversed), bottom right is 19, bottom left is 17
 			.withGains(
-				{0.001, 0, 0.0001}, // distance controller gains
-				{0.001, 0, 0.0001}, // turn controller gains
-				{0.001, 0, 0.0001}  // angle controller gains (helps drive straight)
+				{0.002, 0.00001, 0.00001}, // distance controller gains
+				{0.002, 0.00001, 0.00001}, // turn controller gains
+				{0.002, 0.00001, 0.00001}  // angle controller gains (helps drive straight)
 			)
 			.withSensors(
-				ADIEncoder{'A', 'B'}, // left encoder in ADI ports A & B
-				ADIEncoder{'C', 'D', true},  // right encoder in ADI ports C & D (reversed)
+				ADIEncoder{'C', 'D'}, // left encoder in ADI ports A & B
+				ADIEncoder{'A', 'B', true},  // right encoder in ADI ports C & D (reversed)
 				ADIEncoder{'E', 'F'}  // middle encoder in ADI ports E & F
 			)
 			// green gearset, tracking wheel diameter (2.75 in), center to center distance between wheels (track) (7 in), and TPR (360)
 			// 1 inch middle encoder distance from center of rotation, and 2.75 inch middle wheel diameter
-			.withDimensions(AbstractMotor::gearset::green, {{2.75_in, 7_in, 1_in, 2.75_in}, quadEncoderTPR})
+			.withDimensions(AbstractMotor::gearset::green, {{2.75_in, 17.25_in, 8.25_in, 2.75_in}, quadEncoderTPR})
 			.withOdometry() // use the same scales as the chassis (above)
 			.buildOdometry(); // build an odometry chassis
 
+	Controller masterController;
+
+	auto xModel = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
+
 	while (true) {
-		drive->moveDistance(12_in);
-		drive->turnAngle(90_deg);
+		pros::screen::print(pros::text_format_e_t::E_TEXT_MEDIUM, 0, ("X: " + std::to_string(chassis->getOdometry()->getState().x.getValue())).c_str());
+		pros::screen::print(pros::text_format_e_t::E_TEXT_MEDIUM, 1, ("Y: " + std::to_string(chassis->getOdometry()->getState().y.getValue())).c_str());
+		pros::screen::print(pros::text_format_e_t::E_TEXT_MEDIUM, 2, ("Theta: " + std::to_string(chassis->getOdometry()->getState().theta.getValue())).c_str());
+		xModel->xArcade(
+			masterController.getAnalog(ControllerAnalog::leftX),
+			masterController.getAnalog(ControllerAnalog::leftY),
+			masterController.getAnalog(ControllerAnalog::rightX)
+		);
+
+		if (masterController.getDigital(ControllerDigital::A))
+		{
+			chassis->setState(OdomState{0_m, 0_m, 0_deg});
+		}
+		pros::delay(20);
 	}
+	//drive->moveDistance(12_in);
+	//drive->turnAngle(90_deg);
 }
